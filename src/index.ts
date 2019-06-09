9e3e713178349228219cffbd8eda3758a9ef4e83
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-06-09 15:28:52 -0700

    feat: constructor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export default class DummyClass {
 
+interface IEvent {
+  start_time: Date,
+  end_time: Date
+}
+export default class Series {
+  private _events: IEvent[];
+  constructor(events: IEvent[] = []) {
+    this._events = events;
+  }
+  getEvents() {
+    return this._events;
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,23 @@
-import DummyClass from "../src/index"
+import Series from "../src/index";
+import * as moment from "moment";
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+describe("constructor", () => {
+  it("empty constructor", () => {
+    const series = new Series();
+    expect(series.getEvents()).toEqual([]);
+  });
+  it("constructor with events in it", () => {
+    const events = [
+      {
+          start_time: moment("2019-05-01T03:00:00.000Z").toDate(),
+          end_time: moment("2019-05-01T03:00:00.000Z").toDate()
+      },
+      {
+          start_time: moment("2019-05-01T03:00:00.000Z").toDate(),
+          end_time: moment("2019-05-01T03:00:00.000Z").toDate()
+      }
+    ];
+    const series = new Series(events);
+    expect(series.getEvents()).toEqual(events);
+  });
 })
